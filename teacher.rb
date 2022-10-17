6ff1e5f2295f5825471221c13b76f0ba6a5700bc
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-17 18:43:47 +0300

    Create class Teacher

---
+++ teacher.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknown', parent_permission: false)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
