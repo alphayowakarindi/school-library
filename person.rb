86d185ac6c583e2ecf2f6e6681f3fc309f506cca
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-19 14:19:53 +0300

    Test the decorators

--- person.rb
+++ person.rb
@@ -59,3 +59,10 @@ class TrimmerDecorator < BaseDecorator
     @nameable.correct_name.length <= 10 ? @nameable.correct_name : @nameable.correct_name[0..9]
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
