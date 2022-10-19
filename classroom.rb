c64ab64d83cef151a2f920230e835bc0d1c3ea61
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-19 21:25:43 +0300

    Create classroom class

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  # def add_student()
+  def add_student(student)
+    @students.push(student)
+    student.Classroom = self
+  end
+end
