aa8e4740fffe25dc15b812b660b61d53347c8aa5
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-19 14:11:01 +0300

    Create base decorator

--- person.rb
+++ person.rb
@@ -33,3 +33,15 @@ class Nameable
     raise NotImplementedError
   end
 end
+
+# Base decorator
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
