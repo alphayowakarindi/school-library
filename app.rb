ad4a81610c9fb3308d58e619a0d3fa6e82c8a68f
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-21 02:03:07 +0300

    Create list_all_people method

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 class App
-  attr_accessor :books
+  attr_accessor :books, :people
 
   def initialize
     @books = []
+    @people = []
   end
 
   # list all books
@@ -12,6 +13,17 @@ class App
     else
       @books.each do |book|
         puts "Tilte: \"#{book.title}\", Author: #{book.author}"
+      end
+    end
+  end
+
+  # list all people
+  def list_all_people
+    if @people.empty?
+      puts 'Sorry, the people list is currently empty'
+    else
+      @people.each do |person|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
     end
   end
