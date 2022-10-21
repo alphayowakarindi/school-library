8a728b64626afa1c00e7ed07077e32a54f39d4d4
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-21 03:21:33 +0300

    Update teacher's class constructor

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, name = 'Unknown', parent_permission: false)
+  def initialize(specialization, age, name)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
